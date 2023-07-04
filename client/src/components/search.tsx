e6c80b1812267005838e0b3c409f418a99f133e1
dangminhngo <dangminhngo.dev@gmail.com>
2023-07-04 22:22:19 +0700

    fix: add search clear button

--- client/src/components/search.tsx
+++ client/src/components/search.tsx
@@ -10,6 +10,13 @@ export default function Search() {
   const [keyword, setKeyword] = useState('')
   const { status, data } = useSearch(keyword, { enabled: !!keyword })
 
+  const resetSearch = () => {
+    if (!inputRef.current) return
+
+    inputRef.current.value = ''
+    setKeyword('')
+  }
+
   return (
     <form className="relative">
       <div className="relative">
@@ -22,7 +29,17 @@ export default function Search() {
             setKeyword(e.target.value)
           }, 500)}
         />
-        <Icons.Search className="text-secondary-600 absolute right-4 top-1/2 -translate-y-1/2" />
+        {keyword.length > 0 ? (
+          <button
+            type="button"
+            onClick={resetSearch}
+            className="bg-secondary-700 absolute right-4 top-1/2 grid h-5 w-5 -translate-y-1/2 place-items-center rounded"
+          >
+            <Icons.Clear className="text-secondary-400 h-4 w-4" />
+          </button>
+        ) : (
+          <Icons.Search className="text-secondary-600 absolute right-4 top-1/2 -translate-y-1/2" />
+        )}
       </div>
       {!!data?.length && data.length > 0 && (
         <div className="bg-secondary-800 absolute left-0 top-full mt-2 w-full rounded p-1">
@@ -41,12 +58,7 @@ export default function Search() {
                         : `/teams/${res.id}`
                     }
                     className="hover:bg-secondary-700 flex w-full items-center justify-between rounded px-4 py-2"
-                    onClick={() => {
-                      if (inputRef.current) {
-                        inputRef.current.value = ''
-                      }
-                      setKeyword('')
-                    }}
+                    onClick={resetSearch}
                   >
                     <span>{res.title}</span>
                     <span className="text-sm">{res.type}</span>
