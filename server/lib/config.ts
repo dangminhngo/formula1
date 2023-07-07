881f535a9fbad8a07c7eb615da9e70a9a90f127f
dangminhngo <dangminhngo.dev@gmail.com>
2023-07-08 00:36:10 +0700

    fix: use separated .env files for local development and production, load env files in docker compose

--- server/lib/config.ts
+++ server/lib/config.ts
@@ -1,6 +1,11 @@
 import dotenv from 'dotenv'
 
-dotenv.config()
+dotenv.config({
+  path:
+    process.env.NODE_ENV === 'production'
+      ? '.env'
+      : `.env.${process.env.NODE_ENV}.local`,
+})
 
 const config = {
   CLIENT_URL: process.env.CLIENT_URL,
